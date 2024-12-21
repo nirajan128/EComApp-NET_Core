9026e949e7067d2aab973d758725045d5eb9cf05
Nirajan <nirajanshrestha672@gmail.com>
2024-12-21 10:51:57 -0500

    Project initiated

---
+++ E-Web-NET_CORE/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using E_Web_NET_CORE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Web_NET_CORE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
