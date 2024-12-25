57c402ee2cccc0cbda2fcacc2e7cce11a9f15136
Nirajan <nirajanshrestha672@gmail.com>
2024-12-24 20:47:56 -0500

    added DBContext to the application

---
+++ E-Web-NET_CORE/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_Web_NET_CORE.Data
+{
+    //Must implement DbContext class from entity framework, which is the root class
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //whatever option that are passed to option is passsed to base class of dbcontext
+        {
+            
+        }
+    }
+}
